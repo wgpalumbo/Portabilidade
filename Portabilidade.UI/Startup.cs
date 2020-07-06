82e530a5a7928820be8551d2cfe191fab374aeaf
wgpalumbo <wgpalumbo@yahoo.com.br>
2020-07-06 16:10:23 -0300

    Swagger OK

--- Portabilidade.UI/Startup.cs
+++ Portabilidade.UI/Startup.cs
@@ -15,6 +15,7 @@ namespace Portabilidade.UI
         {
             services.AddMvc();
             services.Add(new ServiceDescriptor(typeof(ISolicitacaoRepository), typeof(SolicitacaoRepository), ServiceLifetime.Singleton));
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,12 +34,24 @@ namespace Portabilidade.UI
                 config.AllowAnyOrigin();
             });
 
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
